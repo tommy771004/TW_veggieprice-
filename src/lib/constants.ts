fe4fc16ba3853f4e986dccdfffd158ad1394bb75
tommy771004 <tommy771004@gmail.com>
2026-05-07 12:14:14 +0800

    [Batch] 20260507 - src/lib/constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
-export const DEFAULT_MARKETS = ['全部市場', '台北一', '台北二', '台中市', '高雄市', '西螺鎮', '彰化市場', '嘉義市', '屏東市']
-export const DEFAULT_HOME_MARKETS = DEFAULT_MARKETS.filter((market) => market !== '全部市場')
+export const ALL_MARKET_SENTINEL = '全部市場'
 export const DEFAULT_MARKET = '台北一'
+export const DEFAULT_MARKETS = [ALL_MARKET_SENTINEL, '台北一', '台北二', '台中市', '高雄市', '西螺鎮', '彰化市場', '嘉義市', '屏東市']
+export const DEFAULT_HOME_MARKETS = DEFAULT_MARKETS.filter((market) => market !== ALL_MARKET_SENTINEL)
